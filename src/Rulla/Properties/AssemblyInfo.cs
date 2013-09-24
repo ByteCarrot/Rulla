0a5b63417bc452d8a8d30010aba27c6a79a500d7
ByteCarrot <support@bytecarrot.com>
2013-09-24 12:46:13 +0200

    Initial commit

---
+++ src/Rulla/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rulla")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ByteCarrot")]
+[assembly: AssemblyProduct("Rulla")]
+[assembly: AssemblyCopyright("Copyright © ByteCarrot 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("89c3a7b7-0f8f-454b-b971-76b7129cf628")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
